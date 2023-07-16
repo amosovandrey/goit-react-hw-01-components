73fa8b93ae1e867dbc2c5b246470cfd011679c28
amosovandrey <rivercity.dp@gmail.com>
2023-07-16 12:05:59 +0300

    task 02 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from '../data.json';
+
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -30,10 +30,14 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   );
 };
 
-Event.propTypes = {
+Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string,
-  stats: PropTypes.object.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
